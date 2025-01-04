4bf2259b6933c7fd6b8cae2950063cb1bbbd73df
yagizerdem <2yagiz004@gmail.com>
2025-01-05 02:13:35 +0300

    approve

--- lib/models/UserModel.dart
+++ lib/models/UserModel.dart
@@ -1,3 +1,5 @@
+import 'dart:ffi';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class UserModel {
@@ -8,6 +10,8 @@ class UserModel {
   final int followingCount;
   final DateTime createdAt;
   final String profileImageUrl;
+  final Array followers;
+  final Array followings;
 
   UserModel(
       {required this.id,
@@ -16,17 +20,22 @@ class UserModel {
       required this.createdAt,
       required this.profileImageUrl,
       required this.followerCount,
-      required this.followingCount});
+      required this.followingCount,
+      required this.followers,
+      required this.followings});
 
   // Factory constructor to create a User object from Firestore data
   factory UserModel.fromJson(String id, Map<String, dynamic> json) {
     return UserModel(
-        id: id,
-        username: json['username'] ?? '',
-        email: json['email'] ?? '',
-        followerCount: json['followerCount'] ?? 0,
-        followingCount: json['followingCount'] ?? 0,
-        createdAt: (json['createdAt'] as Timestamp).toDate(),
-        profileImageUrl: json['profileImageUrl'] ?? '');
+      id: id,
+      username: json['username'] ?? '',
+      email: json['email'] ?? '',
+      followerCount: json['followerCount'] ?? 0,
+      followingCount: json['followingCount'] ?? 0,
+      createdAt: (json['createdAt'] as Timestamp).toDate(),
+      profileImageUrl: json['profileImageUrl'] ?? '',
+      followers: json['followers'] ?? [],
+      followings: json['following'] ?? [],
+    );
   }
 }
--- lib/pages/FriendRequestPage.dart
+++ lib/pages/FriendRequestPage.dart
@@ -110,7 +110,81 @@ class _FrindRequestPageState extends State<FrindRequestPage> {
   }
 
   void Approve(int requestIndex) async {
-    try {} catch (err) {
+    try {
+      FriendRequestModel model = requestList[requestIndex];
+      // get request form db
+      final querySnapshot = await FirebaseFirestore.instance
+          .collection('friendRequests')
+          .where('fromUid', isEqualTo: model.fromUid)
+          .where('destUid', isEqualTo: model.destUid)
+          .where('state',
+              isEqualTo: FriendRequestState.Pending.toString().split('.').last)
+          .get();
+      if (!querySnapshot.docs.isNotEmpty) {
+        throw CustomException("friend request dont exist in database");
+      }
+      String modelFromDbUid = querySnapshot.docs.first.id;
+      FriendRequestModel modelFromDb =
+          FriendRequestModel.fromJson(querySnapshot.docs.first.data());
+
+      // update FrindRequest status to rejected
+      await FirebaseFirestore.instance
+          .collection('friendRequests') // Specify the collection
+          .doc(modelFromDbUid) // Provide the document ID
+          .update({
+        'state': FriendRequestState.Approved.toString().split('.').last,
+      });
+
+      // append to friends  list
+
+      // append to followings
+      var user1DocSnapshot = await FirebaseFirestore.instance
+          .collection('users')
+          .doc(modelFromDb.fromUid) // Query by the 'uid' field
+          .get();
+
+      // append to followers
+      var user2DocSnapshot = await FirebaseFirestore.instance
+          .collection('users')
+          .doc(modelFromDb.destUid) // Query by the 'uid' field
+          .get();
+
+      if (!user1DocSnapshot.exists || !user2DocSnapshot.exists) {
+        throw CustomException("users not exist in database");
+      }
+
+      var user1Doc = user1DocSnapshot.reference;
+      var user2Doc = user2DocSnapshot.reference;
+
+      final batch = FirebaseFirestore.instance.batch();
+
+      batch.update(user1Doc, {
+        'followings': FieldValue.arrayUnion([user2Doc.id]), // Add new follower
+        "followingCount": FieldValue.increment(1),
+      });
+
+      batch.update(user2Doc, {
+        'followers': FieldValue.arrayUnion([user1Doc.id]), // Add new follower
+        "followerCount": FieldValue.increment(1),
+      });
+
+      await batch.commit();
+
+      // successfully done
+      toastification.show(
+        title: const Text(
+          "approved successfully",
+          maxLines: 10,
+          overflow: TextOverflow.ellipsis,
+        ),
+        autoCloseDuration: const Duration(seconds: 5),
+      );
+
+      // remove from list
+      setState(() {
+        requestList.removeAt(requestIndex);
+      });
+    } catch (err) {
       toastification.show(
         title: const Text(
           "internal server error",
