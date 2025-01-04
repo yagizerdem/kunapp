1932f4b4e903a380b9b76651a77fa2bad4817212
yagizerdem <2yagiz004@gmail.com>
2025-01-04 18:05:07 +0300

    other profile page added

--- lib/pages/OtherProfile.dart
+++ lib/pages/OtherProfile.dart
@@ -1,3 +1,5 @@
+import 'package:app/Components/ProfileImage.dart';
+import 'package:app/Components/ProfileStats.dart';
 import 'package:app/models/UserModel.dart';
 import 'package:flutter/material.dart';
 
@@ -13,14 +15,42 @@ class _OtherProfileState extends State<OtherProfile> {
   @override
   Widget build(BuildContext context) {
     String email = widget.usermodel.email; // for test
+    double width = MediaQuery.sizeOf(context).width;
+    double height = MediaQuery.sizeOf(context).height;
+
+    ImageProvider getProfileImage() {
+      String profileImage = widget.usermodel.profileImageUrl;
+      return NetworkImage(profileImage);
+    }
 
     return Scaffold(
       appBar: AppBar(
         title: Text('${widget.usermodel.username}'), // Title of the AppBar
         centerTitle: true, // Center the title
       ),
-      body: Container(
-        child: Text("ohter proifle  $email"),
+      body: Stack(
+        children: [
+          Center(
+            child: Column(
+              children: [
+                ProfileImage(
+                    width: width * .5,
+                    height: width * .5,
+                    imageProvider: getProfileImage()),
+                const SizedBox(
+                  height: 30,
+                ),
+                ProfileStats(
+                  followerCount: widget.usermodel.followerCount,
+                  followingCount: widget.usermodel.followingCount,
+                  userName: widget.usermodel.username,
+                  email: widget.usermodel.email,
+                  spacing: 60,
+                )
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
